18564dce0c49e943caa8f6bc7114bdc4c1fb4d07
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-03-30 01:24:38 +0300

    Implement RollDice component logic

--- src/RollDice.jsx
+++ src/RollDice.jsx
@@ -2,11 +2,29 @@ import React from 'react';
 import './RollDice.css';
 import Die from './Die';
 
+const MAX_NUM = 6;
+
 class RollDice extends React.Component {
+    
+    state = {die1 : 1,
+             die2 : 1}
+
+    roll = () => {
+        this.setState({die1 : this.getNum(),
+                       die2 : this.getNum(),
+                    });
+    }
+
+    getNum() {
+        return Math.floor(Math.random() * MAX_NUM) + 1;
+    }
+    
     render() {
         return (
             <div>
-                <Die num="6"/>
+                <Die num={this.state.die1}/>
+                <Die num={this.state.die2}/>
+                <button onClick={this.roll}>Roll Dice!</button>
             </div>
         );
     }
