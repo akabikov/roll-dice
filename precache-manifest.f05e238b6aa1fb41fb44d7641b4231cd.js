70c475b295b9c89787901fa3c4645dbc9d6d9c60
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-05-02 17:28:30 +0300

    Updates

---
+++ precache-manifest.f05e238b6aa1fb41fb44d7641b4231cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1a290d616f7d0214d290e40043ef3b",
+    "url": "/roll-dice/index.html"
+  },
+  {
+    "revision": "080d40d34e0ae75b5832",
+    "url": "/roll-dice/static/css/main.14f63b1c.chunk.css"
+  },
+  {
+    "revision": "66c8859aea29db8c943d",
+    "url": "/roll-dice/static/js/2.0a423202.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roll-dice/static/js/2.0a423202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080d40d34e0ae75b5832",
+    "url": "/roll-dice/static/js/main.4a3fc0f7.chunk.js"
+  },
+  {
+    "revision": "4a0a27079cb0bf088ea5",
+    "url": "/roll-dice/static/js/runtime-main.b567c7f3.js"
+  }
+]);
